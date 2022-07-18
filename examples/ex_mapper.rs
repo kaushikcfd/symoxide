f53ef2e4bfc8583fdb8a4921f00d3ab39c4590db
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-07-17 22:38:52 -0500

    implemented renamer

--- examples/ex_mapper.rs
+++ examples/ex_mapper.rs
@@ -1,17 +1,27 @@
 use expression_trees::mappers::identity::{IdentityMapper, IdentityMappable};
+use expression_trees::primitives::{Variable, Expression};
 use expression_trees::{variables, add};
+use std::rc::Rc;
 
-struct Copier;
-impl IdentityMapper for Copier {}
-
+struct Renamer;
+impl IdentityMapper for Renamer {
+    fn map_variable(&self, expr: &Variable) -> Rc<dyn Expression> {
+        let new_name = match &expr.name[..] {
+            "x" => "foo",
+            "y" => "bar",
+            _  => panic!("Unknown variable {}", expr),
+        };
+        Rc::new(Variable  {name: new_name.to_string()})
+    }
+}
 
 
 fn main() {
 
-    let copier = Copier {};
+    let renamer = Renamer {};
     let (x, y) = variables!("x y");
 
     let xpy = add(&x, &y);
-    let xpy_copy = xpy.accept(&copier);
-    println!("{}", xpy_copy);
+    let xpy_renamed = xpy.accept(&renamer);
+    println!("{}", xpy_renamed);
 }
