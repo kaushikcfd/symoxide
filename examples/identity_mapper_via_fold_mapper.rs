eb023d86015faa0a7ce9acef53b931b0c3400103
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-07-19 23:13:14 -0500

    adds an example for using fold mappers

--- examples/identity_mapper_via_fold_mapper.rs
+++ examples/identity_mapper_via_fold_mapper.rs
@@ -1,10 +1,13 @@
-use expression_trees::mappers::identity::{IdentityMapper, IdentityMappable};
-use expression_trees::primitives::{Variable, Expression};
+use expression_trees::mappers::fold::{FoldMapper, Foldable};
+use expression_trees::primitives::{Variable, Expression, Sum};
 use expression_trees::{variables, add};
 use std::rc::Rc;
 
 struct Renamer;
-impl IdentityMapper for Renamer {
+
+impl FoldMapper for Renamer {
+    type Output = Rc<dyn Expression>;
+
     fn map_variable(&self, expr: &Variable) -> Rc<dyn Expression> {
         let new_name = match &expr.name[..] {
             "x" => "foo",
@@ -12,6 +15,12 @@ impl IdentityMapper for Renamer {
             _  => panic!("Unknown variable {}", expr),
         };
         Rc::new(Variable  {name: new_name.to_string()})
+    }
+
+    fn map_sum<T1: Foldable, T2: Foldable>(&self, expr: &Sum<T1, T2>) -> Rc<dyn Expression>{
+        let rec_l = expr.l.accept(self);
+        let rec_r = expr.r.accept(self);
+        return Rc::new(Sum {l: rec_l, r: rec_r});
     }
 }
 
