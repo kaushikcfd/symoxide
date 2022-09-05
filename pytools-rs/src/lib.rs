2edc32af463b28d6e91c494211459336308c47a5
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-09-05 12:45:33 -0500

    implements pytools::show_dot

--- pytools-rs/src/lib.rs
+++ pytools-rs/src/lib.rs
@@ -1,3 +1,5 @@
+mod show_graphviz;
 mod unique_name_generator;
 
+pub use show_graphviz::{show_dot, ConvertibleToDotOutputT, DotOutputT};
 pub use unique_name_generator::{make_unique_name_gen, UniqueNameGenerator};
